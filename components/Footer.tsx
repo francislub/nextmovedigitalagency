d517a316df340b98556039eac1e00a7fb1b4c09f
francislub <lubanjwafrancis@gmail.com>
2026-03-08 23:18:24 -0700

    mor hjguyyyi

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -66,25 +66,13 @@ export function Footer() {
                 <img
                   src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/black-iXsseJTvyXimXjpRcnVLUKTT4S6pv2.png"
                   alt="NextMove"
-                  className="block dark:hidden 
-                  w-24 h-12 
-                  sm:w-28 sm:h-14 
-                  md:w-32 md:h-16 
-                  lg:w-40 lg:h-20 
-                  xl:w-48 xl:h-24 
-                  object-contain"
+                  className="block dark:hidden max-w-[150px] sm:max-w-[200px] md:max-w-[250px] lg:max-w-[300px] h-auto object-contain"
                 />
 
                 <img
                   src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/black-iXsseJTvyXimXjpRcnVLUKTT4S6pv2.png"
                   alt="NextMove"
-                  className="block dark:hidden 
-                  w-24 h-12 
-                  sm:w-28 sm:h-14 
-                  md:w-32 md:h-16 
-                  lg:w-40 lg:h-20 
-                  xl:w-48 xl:h-24 
-                  object-contain"
+                  className="hidden dark:block max-w-[150px] sm:max-w-[200px] md:max-w-[250px] lg:max-w-[300px] h-auto object-contain"
                 />
 
               </Link>
